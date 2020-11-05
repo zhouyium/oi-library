a6faff507e6507cbfcca7f006fcc7a09c1bbe85b
周毅 <yc07444@um.edu.mo>
2020-11-05 15:33:54 +0800

    改名为 DSU

--- src/dsu.hpp
+++ src/dsu.hpp
@@ -5,23 +5,26 @@
 
 namespace oi_library {
 
+//Disjoint Set Union
 template <class T>
-struct _DISJOINTSET {
+struct DSU {
+private:
     /*
-    当_ds[x]<0的时候，说明x的父节点是自己。同时 |_ds[x]| 表示集合内元素数量
-    当_ds[x]>0的时候，表示父节点
+    当_dsu[x]<0的时候，说明x的父节点是自己。同时 |_dsu[x]| 表示集合内元素数量
+    当_dsu[x]>0的时候，表示父节点
     */
-    std::vector<T> _ds;//并查集
+    std::vector<T> _dsu;//并查集
     T _n;//数据容量
 
-    _DISJOINTSET(T n) : _n(n),  _ds(n, -1) {}
+public:
+    DSU(T n) : _n(n),  _dsu(n, -1) {}
   
     //查找x的父亲
     T find_root(T x) {
-        if (x<0 || x>=_n || _ds[x]<0) {
+        if (x<0 || x>=_n || _dsu[x]<0) {
             return x;
         }
-        return _ds[x] = find_root(_ds[x]);
+        return _dsu[x] = find_root(_dsu[x]);
     }
  
     //建立关系
@@ -35,12 +38,12 @@ struct _DISJOINTSET {
             //两个已经在同一个集合
             return false;
         }
-        if (_ds[x]>_ds[y]) {
+        if (_dsu[x]>_dsu[y]) {
             //保证不会出现单边增长
             swap(x, y);
         }
-        _ds[x] += _ds[y];
-        _ds[y] = x;
+        _dsu[x] += _dsu[y];
+        _dsu[y] = x;
         return true;
     }
  
@@ -57,7 +60,7 @@ struct _DISJOINTSET {
         if (x<0 || x>=_n) {
             return 0;
         }
-        return -_ds[find_root(x)];
+        return -_dsu[find_root(x)];
     }
 }; //end of struct
 
